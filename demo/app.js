0906c6813ed51d97ce5c0e869fcba1c847c26f18
chriscauley <chris@lablackey.com>
2020-05-07 14:38:37 +0000

    docs

---
+++ demo/app.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { last } from 'query-string'
+import qs from 'query-string'
+
+import RestHook from '../src'
+
+const withCharacterTypes = RestHook('./data/index.json')
+
+const MyComponent = withCharacterTypes(props => {
+  const { loading, character_types } = props.api
+  if (loading) {
+    return null
+  }
+
+  const page = parseInt(
+    qs.parse(window.location.search.slice(1)).page || 1
+  )
+
+  const { slug } = props.match.params
+  const selected_character_type = character_types.find(
+    character_type => character_type.slug === slug
+  )
+
+  return (
+    <div>
+      <div className="flex border-b">
+        {character_types.map( ({ name, slug }) => (
+          <div key={slug}>
+            <Link to={`/${slug}`} className="text-blue-500 p-2">
+              {name}
+            </Link>
+          </div>
+        ))}
+      </div>
+      {selected_character_type && (
+        <CharacterList character_type={selected_character_type} page={page} />
+      )}
+    </div>
+  )
+})
+
+const withCharacters = RestHook('./data/${character_type.slug}-${page}.json')
+
+const CharacterList = withCharacters(props => {
+  const { pages, name, slug } = props.character_type
+  const { loading, results } = props.api
+  const { page } = props
+  if (loading && !results) {
+    return "Loading ..."
+  }
+  return (
+    <div className="max-w-5xl">
+      <Pagination page={page} page_count={pages} />
+      <ul className="ml-6 list-disc">
+        {results.map(character => (
+          <li key={character} className="pb-1">{character}</li>
+        ))}
+      </ul>
+      <Pagination page={page} page_count={pages} />
+    </div>
+  )
+})
+
+
+const Pagination = ({ page, page_count }) => page_count > 1 && (
+  <div className="flex">
+    {page > 1 && (
+      <Link to={"?page="+ (page - 1)} className="p-2 underline text-blue-500">
+        &lt;
+      </Link>
+    )}
+    <span className="p-2">On Page {page} / {page_count}</span>
+    {page < page_count && (
+      <Link to={"?page="+ (page + 1)} className="p-2 underline text-blue-500" disabled>
+        &gt;
+      </Link>
+    )}
+  </div>
+)
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/:slug" component={MyComponent} />
+        <Route exact path="/" component={MyComponent} />
+      </BrowserRouter>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('react-app')
+)
---
+++ demo/make-data.js
@@ -0,0 +1,36 @@
+const pokemon = require('pokemon')
+const fs = require('fs')
+const { range } = require('lodash')
+
+const PER_PAGE = 10
+
+const datasets = [
+  ["Ghost Busters", ["Peter Venkman", "Ray Stantz", "Egon Spengler", "Winston Zeddemore"]],
+  ["Ninja Turtles", ["Leonardo", "Michelangelo", "Donatello", "Raphael"]],
+  ["Pokemon", pokemon.all().sort()],
+]
+
+const files = {
+  'index.json': {character_types: []},
+}
+
+datasets.forEach( args => {
+  const [name, list] = args
+  const slug = name.toLowerCase().replace(' ','')
+  const pages = Math.ceil(list.length/PER_PAGE)
+  files['index.json'].character_types.push({name, slug, pages})
+  files[slug+'.json'] = { results: list.slice(0, PER_PAGE) }
+  range(1,pages+1).forEach(page_no => {
+    const filename = slug+'-'+page_no+'.json'
+    files[filename] = { results: list.slice(page_no*PER_PAGE, (page_no+1)*PER_PAGE) }
+  })
+})
+
+
+const DIR = './docs/data/';
+!fs.existsSync('./docs') && fs.mkdirSync('./docs')
+!fs.existsSync(DIR) && fs.mkdirSync(DIR)
+
+Object.entries(files).forEach(
+  (args) => fs.writeFileSync(DIR+args[0], JSON.stringify(args[1]))
+)
